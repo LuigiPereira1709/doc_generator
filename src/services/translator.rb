7724e45f61505bde576ec7133586035201f409f3
Formação Análise de Dados 2023.1 <36131502023.1@senacrjedu.local>
2024-01-17 16:48:09 -0300

    new features in service translator

--- src/services/translator.rb
+++ src/services/translator.rb
@@ -13,5 +13,24 @@ class Translator
         "destination_language" => destination_language
         )
   end
-  def music(letter, )
+
+  def music(letter, origin_country, destination_language)
+    @nethod = __method__
+    prompt = Jsom.read(@service, @method)
+    prompt['prompt'] = prompt['prompt'].merge(
+      "letter" => letter,
+      "origin_country" => origin_country,
+      "destination_language" => destination_language
+    )
+  end
+
+  def literature(text, origin_country, destination_language)
+    @nethod = __method__
+    prompt = Jsom.read(@service, @method)
+    prompt['prompt'] = prompt['prompt'].merge(
+      "text" => text,
+      "origin_country" => origin_country,
+      "destination_language" => destination_language
+    )
+  end
 end
